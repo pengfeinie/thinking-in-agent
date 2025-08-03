e9dab6f9439237728d43e99a2e6916dda991119c
pfnie <895951186@qq.com>
2025-08-03 16:04:37 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -23,6 +23,15 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+# add "Edit on Github"
+html_context = {
+	"display_github": True,
+	"github_user":"pengfeinie",
+	"github_repo":"thinking-in-langchain",
+	"github_version":"main/source/"
+}
+
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -32,3 +41,17 @@ html_static_path = ['_static']
 html_css_files = [
     'custom.css',
 ]
+
+# If true, links to the reST sources are added to the pages.
+#
+html_show_sourcelink = True
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+#
+html_show_sphinx = True
+
+html_theme_options = {
+    'navigation_depth': 2, #根据实际需求调整
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+}
